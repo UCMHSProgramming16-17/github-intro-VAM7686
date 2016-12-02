8758dde59df562546177a65206dbcf26fe5fbaf8
Vivek Mhatre <vmhatre@ucvts.org>
2016-12-02 15:41:47 +0000

    Dank Memes

---
+++ le_meme/lib/le_meme.rb
@@ -0,0 +1,10 @@
+require 'rmagick'
+require 'word_wrapper'
+
+require 'le_meme/version'
+require 'le_meme/meme'
+require 'le_meme/meme_lib'
+
+module LeMeme
+  IMAGE_EXTENSIONS = /\.(jp[e]?g|png|gif)$/i
+end
---
+++ le_meme/spec/spec_helper.rb
@@ -0,0 +1,4 @@
+require 'pry'
+require 'le_meme'
+
+RSpec.configure(&:raise_errors_for_deprecations!)
